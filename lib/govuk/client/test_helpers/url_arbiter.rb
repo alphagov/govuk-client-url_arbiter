6b49503242c06581b058b98e82aea835fbbc08dd
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-09-03 10:20:51 +0000

    Add some docs for the test helpers

--- lib/govuk/client/test_helpers/url_arbiter.rb
+++ lib/govuk/client/test_helpers/url_arbiter.rb
@@ -3,9 +3,17 @@ require 'plek'
 module GOVUK
   module Client
     module TestHelpers
+      # Some test helpers for the url-arbiter client. This module is expected
+      # to be mixed in to test classes.
+      #
+      # These rely on WebMock being available in the test suite.
       module URLArbiter
         URL_ARBITER_ENDPOINT = Plek.new.find('url-arbiter')
 
+        # Stub out some sensible default url-arbiter responses.
+        #
+        # - all +GET+ requests for paths to return a 404.
+        # - all +PUT+ requests to register a path return a 201 along with corresponding sample data.
         def stub_default_url_arbiter_responses
           stub_request(:get, %r[\A#{URL_ARBITER_ENDPOINT}/paths/]).
             to_return(:status => 404)
@@ -16,6 +24,15 @@ module GOVUK
           }
         end
 
+        # Stub out calls to simulate webmock having registration information
+        # for a given path.
+        #
+        # - +GET+ requests for the path return corresponding sample data.
+        # - +PUT+ requests with a matching publishing_app return 200 along with the sample data.
+        # - +PUT+ requests with a different publishing_app will return a 409 and include error data in the response.
+        #
+        # @param path [String] The path to be reserved.
+        # @param publishing_app [String] The app the path should be registered to.
         def url_arbiter_has_registration_for(path, publishing_app)
           data = url_arbiter_data_for(path, "publishing_app" => publishing_app)
           error_data = data.merge({
@@ -33,6 +50,10 @@ module GOVUK
             to_return(:status => 200, :body => data.to_json, :headers => {:content_type => "application/json"})
         end
 
+        # Generate sample url-arbiter data for a given path.
+        #
+        # @param path [String] The path being requested
+        # @param override_attributes [Hash] Any specific attributes to override the defaults.
         def url_arbiter_data_for(path, override_attributes = {})
           now = Time.now.utc.iso8601
           {
